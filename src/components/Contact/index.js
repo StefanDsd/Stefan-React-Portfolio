7667f0e328e3af37b54bea848976f173b98e1700
StefanDsd <stefan_dascalescu92@gmail.com>
2023-02-17 16:32:43 +0100

    working on contact me functionality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,22 +1,50 @@
-import React from 'react';
-// import { Link } from 'react-router-dom';
-import '../Contact/index.modules.css';
-// import HireMe from "../HireMe";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "../Contact/index.modules.css";
 
+export const Contact = () => {
+  const form = useRef();
 
-const Contact = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-        <div className='contact' id="contact">
-        <h1>Contact me</h1>
-        <input type="text" placeholder='Name'/>
-        <input type="text" placeholder='Email'/>
-        <input type="text" placeholder='Message' className='input-message'/>
-        <div to="/">send</div>
-        {/* <div className='second-lottie'>
-          <HireMe />
-        </div> */}
-  </div>
+    <div className="contact" id="contact">
+      <form className="contact" ref={form} onSubmit={sendEmail}>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Message" className="input-message" />
+      </form>
+    </div>
   );
 };
+
+<div className="contact" id="contact">
+  <h1>Contact me</h1>
+  <input type="text" placeholder="Name" />
+  <input type="text" placeholder="Email" />
+  <input type="text" placeholder="Message" className="input-message" />
+  <div to="/">send</div>
+  {/* <div className='second-lottie'>
+          <HireMe />
+        </div> */}
+</div>;
 
 export default Contact;
